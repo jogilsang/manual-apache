189d5f7249e5715a4f48f44458e2ded88a1355dd
jogilsang <jogilsang@gmail.com>
2020-10-16 22:33:41 +0900

    added : new code

--- code/status_check.sh
+++ code/status_check.sh
@@ -16,8 +16,8 @@
 # List of websites to check
 ##
 
-url="슬랙채널의 웹훅도메인, SlackChannel webhook domain"
-websites_list="상태체크할 도메인 주소"
+url="https://hooks.slack.com/services/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"
+websites_list="https://www.naver.com https://www.google.com https://www.daum.net"
 # websites_list ="your_domain1/test your_domain2 your_domain3"
 
 
@@ -55,7 +55,7 @@ for website in ${websites_list} ; do
         if [[ "$status_code" -ne 200 ]] ; then
             # POST request to Discord Webhook with the domain name and the HTTP status code
             # curl -H "Content-Type: application/json" -X POST -d '{"text":"'"${domain} : ${status_code}"'"}' $url
-            curl -H "Content-Type: application/json" -X POST -d "$(generate_post_data $domain $color $status_code $msg)"  $url
+            curl -H "Content-Type: application/json" -X POST -d "$(generate_post_data ${website} $color $status_code ${website +"장애발생"})"  $url
         else
             echo "${website} is running!"
         fi
